e27363835be248912731405a90d9a760820b9396
koki-develop <kou.pg.0131@gmail.com>
2022-05-12 13:20:13 +0900

    twitter, facebook を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitter: {
+          main: "#1da1f2",
+          dark: "#1470a9",
+        },
+        facebook: {
+          main: "#1877f2",
+          dark: "#1053a9",
+        },
+      },
+    },
   },
   plugins: [],
 };
