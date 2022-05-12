c41f70ac751816637da68cd4b7688df64e13ef26
koki-develop <kou.pg.0131@gmail.com>
2022-05-12 12:52:33 +0900

    meta タグを設定

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,62 @@
-import type { LoaderFunction } from "@remix-run/cloudflare";
+import type { LoaderFunction, MetaFunction } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
+
+const cloudinaryConfig = {
+  cloudName: "koki-develop",
+  cardId: "OGP Messenger/card",
+  fontId: "Sawarabi Gothic",
+};
+
+const buildOgpImageUrl = (text: string | null): string | null => {
+  if (!text) return null;
+  const trimmedText = text.trim();
+  if (trimmedText === "") return null;
+
+  const { cloudName, cardId, fontId } = cloudinaryConfig;
+  return `https://res.cloudinary.com/${encodeURIComponent(
+    cloudName
+  )}/image/upload/c_fit,w_1000,h_480,l_text:${encodeURIComponent(
+    fontId
+  )}_56:${encodeURIComponent(
+    encodeURIComponent(trimmedText)
+  )}/fl_layer_apply/v1/${encodeURIComponent(cardId)}`;
+};
+
+export const meta: MetaFunction = ({ data }) => {
+  const { url, text } = data;
+
+  const siteName = "OGP Messenger";
+
+  const baseTitle = "OGP Messenger";
+  const title = (() => {
+    if (!text) return baseTitle;
+    const trimmedText = text.trim();
+    if (trimmedText === "") return baseTitle;
+    return `${text} | ${baseTitle}`;
+  })();
+
+  const description = "OGP 画像でメッセージを発信できるツール";
+
+  const imageUrl = buildOgpImageUrl(text);
+
+  return {
+    title,
+    "og:title": title,
+
+    description,
+    "og:description": description,
+
+    "og:site_name": siteName,
+    "og:type": "website",
+    "og:locale": "ja_JP",
+    "og:url": url,
+
+    "twitter:card": "summary_large_image",
+    "twitter:site": "@koki_develop",
+    "og:image": imageUrl,
+  };
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
