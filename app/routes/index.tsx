c78158b0131aa07c82ad2e85c009eaa02701796f
koki-develop <kou.pg.0131@gmail.com>
2022-05-12 12:07:58 +0900

    text パラメータを表示するところまで

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,16 @@
+import type { LoaderFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const text = url.searchParams.get("text");
+
+  return json({ text });
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const { text } = useLoaderData<{ text: string | null }>();
+
+  return <div>text: {JSON.stringify(text)}</div>;
 }
