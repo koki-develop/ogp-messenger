146b0a75165eb33691f836e4e6e8254b0bffda36
koki-develop <kou.pg.0131@gmail.com>
2022-05-12 21:23:30 +0900

    eslintrc の設定を eslintrc.js にマージ

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "@remix-run/eslint-config",
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
